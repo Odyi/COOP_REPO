d2d4e39d648a9246b4912583a7f6228847eebcfa
Ulrik Mjønes <151926631+9876543214@users.noreply.github.com>
2024-09-23 11:56:26 +0200

    Add files via upload

--- back_end.py
+++ back_end.py
@@ -1,18 +1,16 @@
-def leggSammen():
-    tall1 = input("Skriv inn det første tallet: ")
-    tall2 = input("Skriv inn det andre tallet: ")
-    sum = int(tall1) + int(tall2)
-    print(f'{tall2} + {tall2} = {sum}')
+import operator
 
+ops = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    "/": operator.truediv
+}
 
-def trekkFra():
-    tall1 = input("Skriv inn det første tallet: ")
-    tall2 = input("Skriv inn det andre tallet: ")
-    diff = int(tall1) - int(tall2)
-    print(f'{tall2} - {tall2} = {diff}')
-
-
-''' Her er noen påbegynte tanker til utvidelse
-def gange():
-    produkt = 3*3
-    TODO: Gjøre ferdig '''
+def regne():
+    tall1 = float(input("Skriv inn tall: "))
+    operasjon = input("Skriv in operasjon(+ - * /): ")
+    tall2 = float(input("skriv inn tall: "))
+    op_func = ops[operasjon]
+    result = op_func(tall1, tall2)
+    print(f"{tall1} {operasjon} {tall2} = {result}")
