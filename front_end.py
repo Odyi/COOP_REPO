6b720a4597c6828386b60248a42e415b418074c3
Odysseas <156892267+Odyi@users.noreply.github.com>
2024-09-24 11:44:37 +0200

    Update front_end.py

--- front_end.py
+++ front_end.py
@@ -1,36 +1,48 @@
-from back_end import *
-
-
+# Funksjon som skriver ut hovedmenyen for kalkulatoren
 def printMeny():
+    # Skriver ut menyen til brukeren
     print("------------------- Kalkulator -------------------")
     print("| 1. Regne                                       |")
     print("| 2. Avslutt                                     |")
     print("--------------------------------------------------")
+    
+    # Brukeren velger et menyvalg ved å skrive inn et tall
     menyvalg = input("Velg operasjon fra menyen: ")
+    
+    # Kaller funksjonen for å utføre den valgte menyoperasjonen
     utfoerMenyvalg(menyvalg)
 
 
+# Funksjon som tar seg av å utføre brukerens menyvalg
 def utfoerMenyvalg(valgtTall):
+    # Hvis brukeren velger "1", utføres regne-operasjonen
     if valgtTall == "1":
-        regne()
+        regne()  # Dette kaller en ekstern funksjon fra "back_end" som må være definert et annet sted
 
+    # Hvis brukeren velger "2", vil de få muligheten til å avslutte programmet
     elif valgtTall == "2":
         bekreftelse = input("Er du sikker på at du vil avslutte? J/N ")
+        
+        # Hvis brukeren bekrefter med "J" eller "j", avsluttes programmet
         if (bekreftelse == "J" or bekreftelse == "j"):
-            exit()
+            exit()  # Avslutter programmet
         else:
+            # Hvis brukeren ikke bekrefter avslutningen, vises menyen igjen
             printMeny()
     else:
-        nyttForsoek = input("*** Ugyldig valg."
-                            "Velg et tall 1 eller 2."
-                            " Trykk for å fortsette *** ")
+        # Hvis brukeren skriver inn et ugyldig valg, får de beskjed om det
+        nyttForsoek = input("*** Ugyldig valg. Velg et tall 1 eller 2. Trykk for å fortsette *** ")
+        # Menyen vises på nytt
         printMeny()
 
 
+# Funksjon som setter programmet på pause til brukeren trykker en tast
 def pause_og_fortsett():
+    # Pause for brukerens input for å fortsette
     input("-- Trykk en tast for å fortsette --")
+    # Viser hovedmenyen igjen
     printMeny()
 
 
+# Kaller hovedfunksjonen for å vise menyen første gang når programmet starter
 printMeny()
-
