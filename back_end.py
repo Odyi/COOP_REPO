e960105b06f52dfd30110f495aed3aea99735715
Ulrik Mjønes <151926631+9876543214@users.noreply.github.com>
2024-09-24 13:30:10 +0200

    Invalid input

--- back_end.py
+++ back_end.py
@@ -1,5 +1,7 @@
 import operator
 
+tall2_input = 0
+
 ops = {
     "+": operator.add,
     "-": operator.sub,
@@ -8,9 +10,32 @@ ops = {
 }
 
 def regne():
-    tall1 = float(input("Skriv inn tall: "))
+    tall1 = input("Skriv inn tall: ")
+    try: 
+        valid = int(tall1)
+    except:
+        print("Ugyldig input")
+        regne()
     operasjon = input("Skriv in operasjon(+ - * /): ")
-    tall2 = float(input("skriv inn tall: "))
+    tall2()
+
+    global tall2_input
+
+    tall1_int = int(tall1)
+    tall2_int = int(tall2_input)
     op_func = ops[operasjon]
-    result = op_func(tall1, tall2)
-    print(f"{tall1} {operasjon} {tall2} = {result}")
+    result = op_func(tall1_int, tall2_int)
+    print(f"{tall1_int} {operasjon} {tall2_int} = {result}")
+
+def tall2():
+    global tall2_input
+    tall2_input = input("Skriv inn tall: ")
+    print(type(tall2_input))
+
+    try: 
+        print("try: " + tall2_input)
+        valid = int(tall2_input)
+    except:
+        print("except: " + tall2_input)
+        input("Ugyldig input, trykk en tast for å fortsette")
+        tall2()
